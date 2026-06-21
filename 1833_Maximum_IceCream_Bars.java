a6df5ed92b339ff4ab5fccef9d2edd782b3742b1
Vanshika Rajput <vanshikaacodes@gmail.com>
2026-06-21 12:31:11 +0530

    Add solution for LeetCode 1833: Maximum Ice Cream Bars

---
+++ 1833_Maximum_IceCream_Bars.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+
+        int count = 0;
+
+        for (int cost : costs) {
+            if (coins >= cost) {
+                coins -= cost;
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+    }
+}
