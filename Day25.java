88e2d7cdb8044b163fd101cdda90a5a4974b6f15
Vanshika Rajput <vanshikaacodes@gmail.com>
2026-07-11 13:07:17 +0530

    Leetcode: 2685 – Count the Number of Complete Components (Java)

---
+++ Day25.java
@@ -0,0 +1,51 @@
+public class Day25{
+    class Solution {
+    List<Integer>[] graph;
+    boolean[] visited;
+    int nodes;
+    int degreeSum;
+
+    public int countCompleteComponents(int n, int[][] edges) {
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        visited = new boolean[n];
+        int complete = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                nodes = 0;
+                degreeSum = 0;
+
+                dfs(i);
+
+                int edgeCount = degreeSum / 2;
+                if (edgeCount == nodes * (nodes - 1) / 2) {
+                    complete++;
+                }
+            }
+        }
+
+        return complete;
+    }
+
+    private void dfs(int node) {
+        visited[node] = true;
+        nodes++;
+        degreeSum += graph[node].size();
+
+        for (int nei : graph[node]) {
+            if (!visited[nei]) {
+                dfs(nei);
+            }
+        }
+    }
+}
+}
